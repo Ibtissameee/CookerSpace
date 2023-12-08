06e964d880b5d13f190b6a6acba95d9d4690d42b
Ibtissameee <129773647+Ibtissameee@users.noreply.github.com>
2023-12-08 12:30:35 +0100

    Add files via upload

---
+++ recyclerview_list.java
@@ -0,0 +1,29 @@
+package com.example.recipe_app_cooker_space_test2;
+
+import androidx.annotation.IntegerRes;
+
+public class recyclerview_list {
+    private Integer image ;
+    private String text;
+
+    public recyclerview_list(Integer image, String text) {
+        this.image = image;
+        this.text = text;
+    }
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
